3dbe927636e4d605389eca85fc5a11249679eaac
Gorokhov Aleksey <car1es@bk.ru>
2023-04-17 18:52:11 +0300

    query

--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -3,6 +3,7 @@ package ru.netology;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
@@ -12,34 +13,25 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
-    final ExecutorService threadPool = Executors.newFixedThreadPool(64);
-    final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
-    private final ConcurrentHashMap<String, ConcurrentHashMap<String, Handler>> handlers = new ConcurrentHashMap<>();
+    private ConcurrentHashMap<String, ConcurrentHashMap<String, Handler>> handlers = new ConcurrentHashMap<>();
+    final List validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+    private ExecutorService thread;
 
-    public void startServer(int port) {
+    public void run(int port) {
         try (final ServerSocket serverSocket = new ServerSocket(port)) {
+            thread = Executors.newFixedThreadPool(64);
             while (true) {
                 final Socket socket = serverSocket.accept();
-                threadPool.submit(() -> connectionHandler(socket));
+                thread.submit(() -> {
+                    try {
+                        response(socket);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
             }
         } catch (IOException ex) {
             ex.printStackTrace();
-        }
-    }
-
-    protected void connectionHandler(Socket socket) {
-        try {
-            final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            final var out = new BufferedOutputStream(socket.getOutputStream());
-            final var requestLine = in.readLine();
-            final var parts = requestLine.split(" ");
-            if (parts.length != 3) {
-                socket.close();
-            } else {
-                response(parts, out);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 
@@ -50,22 +42,20 @@ public class Server {
         handlers.get(method).put(path, handler);
     }
 
-    public void response(String[] parts, BufferedOutputStream out) throws IOException {
-        final var method = parts[0];
-        final var path = parts[1];
+    public void response(Socket socket) throws IOException {
+        final var in = new BufferedInputStream(socket.getInputStream());
+        final var out = new BufferedOutputStream(socket.getOutputStream());
+        Request request = null;
+        try {
+            request = Request.makeRequest(in);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
 
-        Request request;
-        if (method != null) {
-            request = new Request(method, path);
-        } else {
+        if (request == null && !handlers.containsKey(request.getMethod())) {
             notFound(out);
             return;
         }
-        if (!handlers.containsKey(request.getMethod())) {
-            notFound(out);
-            return;
-        }
-
         ConcurrentHashMap<String, Handler> hMap = handlers.get(request.getMethod());
         String reqPath = request.getPath();
         if (hMap.containsKey(reqPath)) {
@@ -75,7 +65,7 @@ public class Server {
             if (!validPaths.contains(request.getPath())) {
                 notFound(out);
             } else {
-                normalResponse(out, path);
+                normalResponse(out, request.getPath());
             }
         }
     }
@@ -128,7 +118,4 @@ public class Server {
         }
 
     }
-
 }
-
-
